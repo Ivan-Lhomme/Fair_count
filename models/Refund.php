a674cba32e6b53246073ba942c3a97707532bf5c
Ivan <i.lhommeageorges@gmail.com>
2025-12-11 16:47:29 +0100

    Add refund class

---
+++ models/Refund.php
@@ -0,0 +1,32 @@
+<?php
+class Refund{
+    public function __construct(private int $amount, private int $ownerId, private int $groupId, private ?int $id = null) {}
+
+    public function getAmount() {
+        return $this->amount;
+    }
+    public function setAmount(int $amount) {
+        $this->amount = $amount;
+    }
+
+    public function getOwnerId() {
+        return $this->ownerId;
+    }
+    public function setOwnerId(int $ownerId) {
+        $this->ownerId = $ownerId;
+    }
+
+    public function getGroupId() {
+        return $this->groupId;
+    }
+    public function setGroupId(int $groupId) {
+        $this->groupId = $groupId;
+    }
+
+    public function getId() {
+        return $this->id;
+    }
+    public function setId(int $id) {
+        $this->id = $id;
+    }
+}
