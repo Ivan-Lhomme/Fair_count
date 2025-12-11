5e73aad1975ba15f17e31c39f46087de5b570745
Ivan <i.lhommeageorges@gmail.com>
2025-12-11 11:14:19 +0100

    Add and finish abstract manager

---
+++ managers/AbstractManager.php
@@ -0,0 +1,24 @@
+<?php
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->load();
+
+abstract class AbstractManager{
+    protected PDO $db;
+
+    public function __construct() {
+        $host = $_ENV["DB_HOST"];
+        $port = $_ENV["DB_PORT"];
+        $dbname = $_ENV["DB_NAME"];
+
+        $connexionString = "mysql:host=$host;port=$port;dbname=$dbname;charset=utf8";
+
+        $user = $_ENV["DB_USER"];
+        $pass = $_ENV["DB_PASS"];
+
+        $this->db = new PDO(
+            $connexionString,
+            $user,
+            $pass
+        );
+    }
+}
