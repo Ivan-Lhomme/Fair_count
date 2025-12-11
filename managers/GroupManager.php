cc6204e40ec6aadc23648a3da5a409ca6049b8d2
Ivan <i.lhommeageorges@gmail.com>
2025-12-11 15:50:04 +0100

    Add group manager

---
+++ managers/GroupManager.php
@@ -0,0 +1,36 @@
+<?php
+class GroupManager extends AbstractManager{
+    public function findOne(int $id) : array {
+        $query = $this->db->prepare("SELECT * FROM groups where id = :id");
+        $parameters = [
+            "id" => $id
+        ];
+        $query->execute($parameters);
+
+        $result = $query->fetch(PDO::FETCH_ASSOC);
+
+        return [
+            "id" => $result["id"],
+            "name" => $result["name"],
+            "ownerId" => $result["owner_id"]
+        ];
+    }
+
+    public function findAll() : array {
+        $query = $this->db->prepare("SELECT * FROM groups");
+        $query->execute();
+
+        $results = $query->fetchAll(PDO::FETCH_ASSOC);
+        $arrayGroups = [];
+
+        foreach ($results as $result) {
+            $arrayGroups[] = [
+                "id" => $result["id"],
+                "name" => $result["name"],
+                "ownerId" => $result["owner_id"]
+            ];
+        }
+
+        return $arrayGroups;
+    }
+}
